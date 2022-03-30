1d0aa2f7b4172675cd613c4252c1eb01bcce107c
xqq2B <84280408+xqq2B@users.noreply.github.com>
2022-03-30 16:34:59 -0600

    script

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function() {
+//     navigator.serviceWorker.register('./sw.js').then(function(registration) {
+//       // Registration was successful
+//       console.log('ServiceWorker registrado correctamente: ', registration.scope);
+//     }, function(err) {
+//       // registration failed :(
+//       console.log('ServiceWorker error al registrarlo: ', err);
+//     });
+//   });
+// }
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('./sw.js').then(function(registration) {
+  navigator.serviceWorker.register('./sw.js', {
+      scope: '.' // <--- THIS BIT IS REQUIRED
+  }).then(function(registration) {
       // Registration was successful
-      console.log('ServiceWorker registrado correctamente: ', registration.scope);
-    }, function(err) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }, function(err) {
       // registration failed :(
-      console.log('ServiceWorker error al registrarlo: ', err);
-    });
+      console.log('ServiceWorker registration failed: ', err);
   });
 }
